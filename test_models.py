6e54bedeb992ca19125bcddf223f861a7cd25147
shaoanlu <shaoanlu@gmail.com>
2023-04-20 23:00:24 +0900

    add tests

---
+++ test_models.py
@@ -0,0 +1,41 @@
+import unittest
+import numpy as np
+from models import SimpleRobotDynamics
+
+
+class TestSimpleRobotModel(unittest.TestCase):
+    def setUp(self):
+        self.model = SimpleRobotDynamics(np.array([0, 0]))
+
+    def test_init(self):
+        self.assertTrue(np.all(self.model.x == np.array([0, 0])))
+        self.assertEqual(self.model.xr, 0)
+        self.assertEqual(self.model.yr, 0)
+        self.assertEqual(self.model.size, 30)
+
+    def test_forward(self):
+        self.model.forward(np.array([1, 2]))
+        self.assertTrue(np.all(self.model.x == np.array([1, 2])))
+
+    def test_x_dot(self):
+        x_dot = self.model.x_dot(np.array([1, 2]))
+        self.assertTrue(np.all(x_dot == np.array([1, 2])))
+
+    def test_h(self):
+        h_val = self.model.h(np.array([0, 0]))
+        self.assertEqual(h_val, -((self.model.size * 2.3) ** 2))
+
+    def test_h_dot(self):
+        h_dot_val = self.model.h_dot(np.array([0, 0]))
+        self.assertTrue(np.all(h_dot_val == np.array([0, 0])))
+
+    def test_update_params(self):
+        params = {"xr": 10, "yr": 20, "size": 40}
+        self.model.update_params(params)
+        self.assertEqual(self.model.xr, 10)
+        self.assertEqual(self.model.yr, 20)
+        self.assertEqual(self.model.size, 40)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_robot.py
@@ -0,0 +1,66 @@
+import unittest
+import numpy as np
+from robot import Robot, K_LEFT, K_RIGHT, K_UP, K_DOWN
+
+
+class DummySimpleRobotDynamics:
+    def __init__(self, x0) -> None:
+        self.x = x0
+
+    def forward(self, u):
+        self.x += u
+
+
+class TestRobot(unittest.TestCase):
+    def setUp(self):
+        self.simple_robot_dynamics = DummySimpleRobotDynamics(np.array([0, 0]))
+        self.robot = Robot(self.simple_robot_dynamics)
+
+    def test_init(self):
+        self.assertEqual(self.robot.x, 0)
+        self.assertEqual(self.robot.y, 0)
+
+    def test_update_positions(self):
+        ux, uy = 1, 1
+        self.robot._update_positions(ux, uy)
+        self.assertEqual(self.robot.x, 1)
+        self.assertEqual(self.robot.y, 1)
+
+    def test_update_nominal_control(self):
+        self.robot._update_nominal_control(K_LEFT)
+        self.assertEqual(self.robot.nominal_ux, -self.robot.vel)
+        self.assertEqual(self.robot.nominal_uy, 0)
+
+        self.robot._update_nominal_control(K_RIGHT)
+        self.assertEqual(self.robot.nominal_ux, self.robot.vel)
+        self.assertEqual(self.robot.nominal_uy, 0)
+
+        self.robot._update_nominal_control(K_UP)
+        self.assertEqual(self.robot.nominal_ux, 0)
+        self.assertEqual(self.robot.nominal_uy, -self.robot.vel)
+
+        self.robot._update_nominal_control(K_DOWN)
+        self.assertEqual(self.robot.nominal_ux, 0)
+        self.assertEqual(self.robot.nominal_uy, self.robot.vel)
+
+    def test_apply_nominal_control(self):
+        self.robot._update_nominal_control(K_LEFT)
+        self.robot._apply_nominal_control()
+        self.assertEqual(self.robot.ux, -self.robot.vel)
+        self.assertEqual(self.robot.uy, 0)
+
+    def test_detect_collision(self):
+        obj1 = Robot(DummySimpleRobotDynamics(np.array([30, 0])))
+        obj2 = Robot(DummySimpleRobotDynamics(np.array([0, 70])))
+        collision_objects = [obj1, obj2]
+
+        self.robot.detect_collision(collision_objects)
+        self.assertTrue(self.robot.is_collided)
+
+        self.robot._update_positions(100, 0)
+        self.robot.detect_collision(collision_objects)
+        self.assertFalse(self.robot.is_collided)
+
+
+if __name__ == "__main__":
+    unittest.main()
